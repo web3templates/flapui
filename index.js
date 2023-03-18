cfd985aea40d071db27ff73d561cdb213ad43e39
Surjith S M <surjithctly@gmail.com>
2023-03-18 19:37:06 +0530

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("Hello, world!");
 console.log(
-  "Flap UI is an Open Source Component Library & Design System for HTML, Astro & React built on top of Tailwind CSS. Speed up your development process with 100s of components with easyness of Charkra UI"
+  "Flap UI is an Open Source Component Library & Design System for HTML, Astro & React built on top of Tailwind CSS. Speed up your development process with 100s of Web & UI components with easyness of Charkra UI"
 );
