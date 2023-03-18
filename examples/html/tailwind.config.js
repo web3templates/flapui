e1e86db6c8129449f208b6719372208d39b6915f
Surjith S M <surjithctly@gmail.com>
2023-03-18 20:24:49 +0530

    presets

---
+++ examples/html/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require("flapui/base")],
+  content: ["./src/**/*.{html,js}"],
+  // your overrides here
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
